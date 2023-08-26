612804c6e2a78228cd156d48be7e2aa07e4df123
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-08-26 09:27:46 -0700

    edit

--- sort.h
+++ sort.h
@@ -23,7 +23,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void quick_sorted(int *array,int lower, int upper, size_t size);
+void quick_sorted(int *array, int lower, int upper, size_t size);
 int partition(int *array, int lower, int upper, size_t size);
 void swap_ints(int *a, int *b);
 void shell_sort(int *array, size_t size);
